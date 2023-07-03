f3d430b7aca324c3d9ee272c3f7e43c5908bf255
Daniel Antunes Ribeiro <38988105+dielito2010@users.noreply.github.com>
2023-07-03 13:01:42 -0400

    modification to include icons in the app list

--- app/src/main/java/br/dev/danielribeiro/listapps/AppAdapter.java
+++ app/src/main/java/br/dev/danielribeiro/listapps/AppAdapter.java
@@ -1,64 +1,80 @@
 package br.dev.danielribeiro.listapps;
 
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.RadioButton;
-import android.widget.RadioGroup;
+import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
 import java.util.List;
 
 public class AppAdapter extends RecyclerView.Adapter<AppAdapter.AppViewHolder> {
 
-    private List<String> appNames;
+    private Context context;
+    private List<ApplicationInfo> appList;
     private int selectedPosition = RecyclerView.NO_POSITION;
 
-    public AppAdapter(List<String> appNames) {
-        this.appNames = appNames;
+    public AppAdapter(Context context, List<ApplicationInfo> appList) {
+        this.context = context;
+        this.appList = appList;
     }
 
     @NonNull
     @Override
     public AppViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_app, parent, false);
-        final AppViewHolder viewHolder = new AppViewHolder(view);
-        viewHolder.radioButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int position = viewHolder.getAdapterPosition();
-                if (position != RecyclerView.NO_POSITION) {
-                    selectedPosition = position;
-                    notifyDataSetChanged();
-                }
-            }
-        });
-        return viewHolder;
+        return new AppViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull AppViewHolder holder, int position) {
-        String appName = appNames.get(position);
-        holder.radioButton.setText(appName);
+        ApplicationInfo appInfo = appList.get(position);
+        PackageManager packageManager = context.getPackageManager();
+        Drawable appIcon = appInfo.loadIcon(packageManager);
+        String appName = appInfo.loadLabel(packageManager).toString();
+
+        holder.appIconImageView.setImageDrawable(appIcon);
+        holder.appNameTextView.setText(appName);
+
         holder.radioButton.setChecked(position == selectedPosition);
+        holder.radioButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectedPosition = holder.getAdapterPosition();
+                notifyDataSetChanged();
+            }
+        });
     }
 
     @Override
     public int getItemCount() {
-        return appNames.size();
-    }
-
-    public class AppViewHolder extends RecyclerView.ViewHolder {
-
-        RadioButton radioButton;
-
-        public AppViewHolder(@NonNull View itemView) {
-            super(itemView);
-            radioButton = itemView.findViewById(R.id.radioButton);
-        }
+        return appList.size();
     }
 
     public int getSelectedPosition() {
         return selectedPosition;
     }
+
+    public class AppViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView appIconImageView;
+        TextView appNameTextView;
+        RadioButton radioButton;
+
+        public AppViewHolder(@NonNull View itemView) {
+            super(itemView);
+            appIconImageView = itemView.findViewById(R.id.appIconImageView);
+            appNameTextView = itemView.findViewById(R.id.appNameTextView);
+            radioButton = itemView.findViewById(R.id.radioButton);
+        }
+    }
 }
+
--- app/src/main/java/br/dev/danielribeiro/listapps/MainActivity.java
+++ app/src/main/java/br/dev/danielribeiro/listapps/MainActivity.java
@@ -10,56 +10,54 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private RecyclerView appRecyclerView;
+    private List<ApplicationInfo> appList;
+    private RecyclerView recyclerView;
+    private AppAdapter appAdapter;
+    private Button saveButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        appRecyclerView = findViewById(R.id.appRecyclerView);
+        // Inicialize a lista de ApplicationInfo
+        appList = new ArrayList<>();
 
-        // Configurar o layout manager do RecyclerView
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
-        appRecyclerView.setLayoutManager(layoutManager);
-
-        // Obter o PackageManager
+        // Obtenha a lista de aplicativos instalados
         PackageManager packageManager = getPackageManager();
-
-        // Obter a lista de aplicativos instalados
         List<ApplicationInfo> installedApps = packageManager.getInstalledApplications(0);
-
-        // Lista de nomes de aplicativos
-        List<String> appNames = new ArrayList<>();
-
         for (ApplicationInfo appInfo : installedApps) {
-            // Filtrar aplicativos do sistema, se desejar
             if ((appInfo.flags & ApplicationInfo.FLAG_SYSTEM) == 0) {
-                String appName = appInfo.loadLabel(packageManager).toString();
-                appNames.add(appName);
+                appList.add(appInfo);
             }
         }
 
-        // Configurar o adaptador do RecyclerView
-        AppAdapter appAdapter = new AppAdapter(appNames);
-        appRecyclerView.setAdapter(appAdapter);
+        // Configure o RecyclerView
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        appAdapter = new AppAdapter(this, appList);
+        recyclerView.setAdapter(appAdapter);
 
-        Button saveButton = findViewById(R.id.saveButton);
+        // Configure o botão de salvar
+        saveButton = findViewById(R.id.saveButton);
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 int selectedPosition = appAdapter.getSelectedPosition();
                 if (selectedPosition != RecyclerView.NO_POSITION) {
-                    String selectedAppName = appNames.get(selectedPosition);
-                    Toast.makeText(getApplicationContext(), selectedAppName, Toast.LENGTH_SHORT).show();
+                    ApplicationInfo selectedApp = appList.get(selectedPosition);
+                    String appName = selectedApp.loadLabel(packageManager).toString();
+                    Toast.makeText(MainActivity.this, "App selecionado: " + appName, Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Nenhum app selecionado", Toast.LENGTH_SHORT).show();
                 }
             }
         });
-
     }
 }
