3384c14caca6a6d4626506afc5546f76de690a49
Daniel Antunes Ribeiro <38988105+dielito2010@users.noreply.github.com>
2023-08-03 15:28:38 -0400

    rearranging folders

---
+++ AndroidApp/app/src/main/java/br/dev/danielribeiro/listapps/AppAdapter.java
@@ -0,0 +1,80 @@
+package br.dev.danielribeiro.listapps;
+
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.graphics.drawable.Drawable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class AppAdapter extends RecyclerView.Adapter<AppAdapter.AppViewHolder> {
+
+    private Context context;
+    private List<ApplicationInfo> appList;
+    private int selectedPosition = RecyclerView.NO_POSITION;
+
+    public AppAdapter(Context context, List<ApplicationInfo> appList) {
+        this.context = context;
+        this.appList = appList;
+    }
+
+    @NonNull
+    @Override
+    public AppViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_app, parent, false);
+        return new AppViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull AppViewHolder holder, int position) {
+        ApplicationInfo appInfo = appList.get(position);
+        PackageManager packageManager = context.getPackageManager();
+        Drawable appIcon = appInfo.loadIcon(packageManager);
+        String appName = appInfo.loadLabel(packageManager).toString();
+
+        holder.appIconImageView.setImageDrawable(appIcon);
+        holder.appNameTextView.setText(appName);
+
+        holder.radioButton.setChecked(position == selectedPosition);
+        holder.radioButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectedPosition = holder.getAdapterPosition();
+                notifyDataSetChanged();
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return appList.size();
+    }
+
+    public int getSelectedPosition() {
+        return selectedPosition;
+    }
+
+    public class AppViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView appIconImageView;
+        TextView appNameTextView;
+        RadioButton radioButton;
+
+        public AppViewHolder(@NonNull View itemView) {
+            super(itemView);
+            appIconImageView = itemView.findViewById(R.id.appIconImageView);
+            appNameTextView = itemView.findViewById(R.id.appNameTextView);
+            radioButton = itemView.findViewById(R.id.radioButton);
+        }
+    }
+}
+
---
+++ AndroidApp/app/src/main/java/br/dev/danielribeiro/listapps/SystemPropertiesHelper.java
@@ -0,0 +1,86 @@
+package br.dev.danielribeiro.listapps;
+
+import static androidx.core.graphics.TypefaceCompatUtil.closeQuietly;
+
+import android.util.Log;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+
+public class SystemPropertiesHelper {
+    public static final String GETPROP_EXECUTABLE_PATH = "/system/bin/getprop";
+    private static final String SETPROP_EXECUTABLE_PATH = "/system/bin/setprop";
+
+    public static String read(String propName) {
+        Process process = null;
+        BufferedReader bufferedReader = null;
+        try {
+            process = new ProcessBuilder().command(GETPROP_EXECUTABLE_PATH, propName).redirectErrorStream(true).start();
+            bufferedReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line = bufferedReader.readLine();
+            if (line == null) {
+                line = ""; //prop not set
+            }
+            Log.i("SystemProperties", "Reading prop:" + propName + " value:" + line);
+            return line;
+        } catch (Exception e) {
+            Log.e("SystemProperties", "Failed to read System Property " + propName, e);
+            return "";
+        } finally {
+            if (bufferedReader != null) {
+                try {
+                    bufferedReader.close();
+                } catch (IOException e) {
+                }
+            }
+            if (process != null) {
+                process.destroy();
+            }
+        }
+    }
+
+    public static void write(String Key, String value){ //throws NoSuchMethodException, InvocationTargetException, IllegalAccessException, ClassNotFoundException {
+
+        Log.i("SystemProperties", "Set prop");
+        InputStreamReader in = null;
+        BufferedReader reader = null;
+        try {
+            Process proc = Runtime.getRuntime().exec("/system/bin/setprop "+Key+" "+value);
+            in = new InputStreamReader(proc.getInputStream());
+            reader = new BufferedReader(in);
+
+            String line = null;
+            Log.d("Saurabh Shell" ,"<OUTPUT>");
+            while ( (line = reader.readLine()) != null)
+                Log.d("Shell" , line);
+            Log.d("Saurabh Shell", "</OUTPUT>");
+            int exitVal = proc.waitFor();
+            Log.d("Saurabh Shell","Process exitValue: " + exitVal);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            closeQuietly(in);
+            closeQuietly(reader);
+        }
+
+
+      /*  Log.i("SystemProperties", "Set prop");
+
+        Class clazz = null;
+        clazz = Class.forName("android.os.SystemProperties");
+
+        Method method = clazz.getDeclaredMethod("set", String.class, String.class);
+        String prop = (String) method.invoke('2', "vold.has_adoptable");
+
+        Log.e("so_test", "my prop is: <" + prop + ">");
+*/
+    }
+
+}
